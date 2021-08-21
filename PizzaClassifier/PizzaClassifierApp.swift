46af0ca151bf2311fb32ecab0e3220f6b71c1762
mtfum <f.yamanaka1127@gmail.com>
2021-08-21 10:11:40 -0700

    Initial Commit

---
+++ PizzaClassifier/PizzaClassifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  PizzaClassifierApp.swift
+//  PizzaClassifier
+//
+//  Created by Fumiya Yamanaka on 2021/08/21.
+//
+
+import SwiftUI
+
+@main
+struct PizzaClassifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
